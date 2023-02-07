099e3a6690aba4b8a1294e52368595a1973d6daf
Brian Marin Silva <marinbrian.as@gmail.com>
2023-02-06 19:14:42 -0600

    Added navigation when clicking on a restaurant and other small fixes.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -15,27 +15,28 @@ const filterByPrice = (price) => {
 };
 
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       { errorMsg ? <Text>{errorMsg}</Text> : null }
-      <Text>We have found {results.length} results</Text>
-      <ResultsList
-        title='Cost Effective'
-        results={filterByPrice('$')}
-      />
-      <ResultsList
-        title='Bit Pricier'
-        results={filterByPrice('$$')}
-      />
-      <ResultsList
-        title='Big Spender'
-        results={filterByPrice('$$$' || '$$$$')}
-      />
-    </View>
+      <ScrollView>
+        <ResultsList
+          title='Cost Effective'
+          results={filterByPrice('$')}
+        />
+        <ResultsList
+          title='Bit Pricier'
+          results={filterByPrice('$$')}
+        />
+        <ResultsList
+          title='Big Spender'
+          results={filterByPrice('$$$' || '$$$$')}
+        />
+      </ScrollView>
+    </>
   );
 };
 
