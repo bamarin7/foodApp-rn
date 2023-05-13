c5757f6c831ddbd28dafe429de9863b8b3fc9cbb
Brian Marin Silva <marinbrian.as@gmail.com>
2023-05-13 10:50:38 -0500

    Updaing api file

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL:'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization: 'Bearer bGmn2yFpOOOpxPgvKzpFFK9kLSCQJfaajf1Nr0gZuPn62dCl5iK6KcXYTRi0oKMODHO_0ZMW9EHzPRQtvTbdBKfI3xCv7R-bnrcQdc1Bcs0nGgvieALQu-jauDbcY3Yx'
-  }
-});
